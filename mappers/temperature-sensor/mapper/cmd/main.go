ee8b17d6a81cb12e28a72a13ee9f8f1471e339c5
Daniel Li <2067089451@qq.com>
2025-07-28 21:33:22 +0800

    Add a temperature sensor mapper
    Signed-off-by: Daniel Li <2067089451@qq.com>

---
+++ mappers/temperature-sensor/mapper/cmd/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+
+	"k8s.io/klog/v2"
+
+	"github.com/kubeedge/mapper-framework/pkg/common"
+	"github.com/kubeedge/mapper-framework/pkg/config"
+	"github.com/kubeedge/mapper-framework/pkg/grpcclient"
+	"github.com/kubeedge/mapper-framework/pkg/grpcserver"
+	"github.com/kubeedge/mapper-framework/pkg/httpserver"
+
+	"github.com/kubeedge/temperature-sensor-mapper/data/device"
+	// modbus_simulator "github.com/kubeedge/temperature-sensor-mapper/modbus-simulator"
+)
+
+func main() {
+	var err error
+	var c *config.Config
+	// go modbus_simulator.InitModbusSimulator(":5502")
+
+	klog.InitFlags(nil)
+	defer klog.Flush()
+
+	if c, err = config.Parse(); err != nil {
+		klog.Fatal(err)
+	}
+	// klog.Infof("config: %+v", c)
+	
+
+	klog.Infoln("Mapper will register to edgecore")
+	deviceList, deviceModelList, err := grpcclient.RegisterMapper(true)
+	if err != nil {
+		klog.Fatal(err)
+	}
+	klog.Infoln("Mapper register finished")
+
+	panel := device.NewDevPanel()
+	err = panel.DevInit(deviceList, deviceModelList)
+	if err != nil && !errors.Is(err, device.ErrEmptyData) {
+		klog.Fatal(err)
+	}
+	// if errors.Is(err, device.ErrEmptyData) {
+	// 	klog.Infoln("devInit finished,but no device data")
+	// }else{
+	// 	klog.Infoln("devInit finished")
+	// }
+	go panel.DevStart()
+
+	// start http server
+	httpServer := httpserver.NewRestServer(panel, c.Common.HTTPPort)
+	go httpServer.StartServer()
+
+	// start grpc server
+	grpcServer := grpcserver.NewServer(
+		grpcserver.Config{
+			SockPath: c.GrpcServer.SocketPath,
+			Protocol: common.ProtocolCustomized,
+		},
+		panel,
+	)
+	defer grpcServer.Stop()
+	if err = grpcServer.Start(); err != nil {
+		klog.Fatal(err)
+	}
+
+}
---
+++ mappers/temperature-sensor/mapper/driver/driver.go
@@ -0,0 +1,200 @@
+package driver
+
+import (
+	"encoding/binary"
+	"fmt"
+	"sync"
+
+	"github.com/kubeedge/mapper-framework/pkg/common"
+	"github.com/kubeedge/mappers-go/pkg/driver/modbus"
+	"github.com/spf13/cast"
+	"k8s.io/klog/v2"
+)
+
+func NewClient(protocol ProtocolConfig) (*CustomizedClient, error) {
+	client := &CustomizedClient{
+		ProtocolConfig: protocol,
+		deviceMutex:    sync.Mutex{},
+		ModbusClient:   nil,
+	}
+	return client, nil
+}
+
+// InitDevice:init modbus client
+func (c *CustomizedClient) InitDevice() error {
+	var config interface{}
+	klog.Infoln("Modbus CommunicateMode:", c.ProtocolConfig)
+	switch c.ProtocolConfig.ConfigData.CommunicateMode {
+	case "TCP":
+		config = modbus.ModbusTCP{
+			SlaveID:  byte(c.ConfigData.SlaveID),
+			DeviceIP: c.ConfigData.IP,
+			TCPPort:  c.ConfigData.Port,
+		}
+	case "RTU":
+		config = modbus.ModbusRTU{
+			SlaveID:      byte(c.ConfigData.SlaveID),
+			SerialName:   c.ConfigData.SerialName,
+			BaudRate:     c.ConfigData.BaudRate,
+			DataBits:     c.ConfigData.DataBits,
+			StopBits:     c.ConfigData.StopBits,
+			Parity:       c.ConfigData.Parity,
+			RS485Enabled: c.ConfigData.RS485Enabled,
+		}
+	default:
+		klog.Errorf("Invalid CommunicateMode: %s", c.ConfigData.CommunicateMode)
+	}
+	klog.Infoln("Start InitDevice with config:", config)
+	klog.Infoln("ConfigType:", fmt.Sprintf("%T", config))
+	modbusClient, err := modbus.NewClient(config)
+	if err != nil {
+		klog.Errorf("Failed to create Modbus client: %v", err)
+		return err
+	}
+	
+	if err := modbusClient.Client.Connect(); err != nil {
+		klog.Errorf("Failed to connect to Modbus device: %v", err)
+		return err
+	}
+	c.ModbusClient = modbusClient
+	klog.Infoln("InitDevice success")
+	return nil
+}
+
+// GetDeviceData :get data from modbus client and normalize data
+func (c *CustomizedClient) GetDeviceData(visitor *VisitorConfig) (interface{}, error) {
+	c.deviceMutex.Lock()
+	defer c.deviceMutex.Unlock()
+
+	klog.Infof("Start reading data from the device,Register: %s, Offset: %d, Limit: %d", visitor.Register, visitor.Offset, visitor.Limit)
+	data, err := c.ModbusClient.Get(visitor.Register, visitor.Offset, visitor.Limit)
+	if err != nil {
+		klog.Errorf("Failed to read data from the device: %v", err)
+		return nil, err
+	}
+	var value uint16
+	var warn string
+	switch visitor.Register {
+	case CoilRegister, InputRegister, DiscreteInputRegister:
+		//The returned Coil register data occupies 1 byte
+		value = uint16(data[0])
+		// Working status warning processing
+		if value == 0 {
+			warn = "The temperature sensor has stopped working"
+		}
+	case HoldingRegister:
+		// The returned Holding register data occupies 2 bytes
+		value = binary.BigEndian.Uint16(data)
+		t := ZoomIn(value, visitor.Scale)
+		// Temperature warning handling
+		if visitor.Min != nil && t < *visitor.Min {
+			warn = fmt.Sprintf("The current temperature is less than the minimum value: %.1f < %.1f", t, *visitor.Min)
+		}
+		if visitor.Max != nil && t > *visitor.Max {
+			warn = fmt.Sprintf("The current temperature is greater than the maximum value: %.1f > %.1f", t, *visitor.Max)
+		}
+	}
+
+	v, err := DataNormalize(value, visitor)
+	var out string
+	if warn == "" {
+		out = fmt.Sprintf("The data read from the device,Type:%T Value:%v", v, v)
+	} else {
+		out = fmt.Sprintf("The data read from the device,Type:%T Value:%v,Warning:%s", v, v, warn)
+	}
+	klog.Infof(out)
+	return out, err
+}
+
+// ZoomIn: zoom in data
+func ZoomIn(data uint16, scale float64) float64 {
+	return cast.ToFloat64(data) * scale
+}
+
+// ZoomOut: zoom out data
+func ZoomOut(data float64, scale float64) uint16 {
+	if scale == 0 {
+		return 0
+	}
+	return cast.ToUint16(data / scale)
+}
+
+// DataNormalize: data normalization
+func DataNormalize(data interface{}, visitor *VisitorConfig) (interface{}, error) {
+
+	switch visitor.DataType {
+	case INT:
+		return cast.ToInt(data), nil
+	case STRING:
+		return cast.ToString(data), nil
+	case FLOAT:
+		v := cast.ToFloat32(data)
+
+		if visitor.Scale != 0 {
+			v = v * cast.ToFloat32(visitor.Scale)
+		}
+		return v, nil
+	case DOUBLE:
+		v := cast.ToFloat64(data)
+
+		if visitor.Scale != 0 {
+			v = v * visitor.Scale
+		}
+		return v, nil
+	case BYTES:
+		return data, nil
+	case BOOL:
+		return cast.ToBool(data), nil
+	}
+	return nil, fmt.Errorf("unsupported data type: %v", visitor.DataType)
+
+}
+
+// DeviceDataWrite: External API call to write data to the device
+func (c *CustomizedClient) DeviceDataWrite(visitor *VisitorConfig, deviceMethodName string, propertyName string, data interface{}) error {
+	c.deviceMutex.Lock()
+	defer c.deviceMutex.Unlock()
+
+	klog.Infof("start write data to device, method name: %s, property name: %s, data: %v, register: %s", deviceMethodName, propertyName, data, visitor.Register)
+
+	return c.SetDeviceData(data, visitor)
+}
+
+// SetDeviceData: Write data internally to the device
+func (c *CustomizedClient) SetDeviceData(data interface{}, visitor *VisitorConfig) error {
+	klog.Infof("start write data to device, data: %v,visitor: %v", data, visitor)
+	if data == "" || data == nil {
+		klog.Infof("data is empty, do not need to write to device")
+		return nil
+	}
+
+	value := cast.ToUint16(ZoomOut(cast.ToFloat64(data), visitor.Scale))
+
+	res, err := c.ModbusClient.Set(visitor.Register, visitor.Offset, value)
+	if err != nil {
+		klog.Errorf("fail to write to the device: %v", err)
+		return err
+	}
+	klog.Infof("write data to device success, value: %v", binary.BigEndian.Uint16((res)))
+	return nil
+}
+
+// StopDevice: stop modbus client
+func (c *CustomizedClient) StopDevice() error {
+	err := c.ModbusClient.Client.Close()
+	if err != nil {
+		klog.Errorf("Failed to close Modbus client: %v", err)
+		return err
+	}
+	return nil
+}
+
+// GetDeviceStates: check the status of device
+func (c *CustomizedClient) GetDeviceStates() (string, error) {
+	klog.Infoln("start to check device status")
+	if err := c.ModbusClient.Client.Connect(); err != nil {
+		klog.Errorf("fail to connect to device: %v", err)
+		return common.DeviceStatusDisCONN, err
+	}
+	return common.DeviceStatusOK, nil
+}
